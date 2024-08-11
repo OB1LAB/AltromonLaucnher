2239cb07b7584cf10bc7f19ddd77ab3505c58496
OB1LAB <ob1chaam@gmail.com>
2024-08-11 19:01:20 +0300

    1.4

--- src/pages/componentsAuth/ExternalInfo/ExternalInfo.jsx
+++ src/pages/componentsAuth/ExternalInfo/ExternalInfo.jsx
@@ -1,14 +1,26 @@
 import styles from "./ExternalInfo.module.css";
+const shell = window.require("electron").shell;
 const ExternalInfo = () => {
   return (
     <div className={styles.socials}>
-      <img src="./discord.svg" width={48} height={48} alt="discord" />
+      <img
+        onClick={() => {
+          shell.openExternal("https://discord.gg/9hWNXv2FpY");
+        }}
+        src="./discord.svg"
+        width={48}
+        height={48}
+        alt="discord"
+      />
       <img
         src="./OB1LAB.png"
         width={48}
         height={48}
         alt="site"
         className={styles.round}
+        onClick={() => {
+          shell.openExternal("https://altromon.ob1lab.ru");
+        }}
       />
     </div>
   );
